e2297a04977951dc60429c5c41c0b3fd08412380
skwara96 <skwara96@gmail.com>
2016-11-01 00:12:02 +0100

    changes in menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $('.hamburger-icon').click(function(){
-        $(this).toggleClass('open');
+        $(this).toggleClass('active');
+         $('.menu-list').toggleClass('active');
+         $('.menu-title').toggleClass('active');
+        //$('.menu-sidebar').toggleClass('active');
     });
 });
